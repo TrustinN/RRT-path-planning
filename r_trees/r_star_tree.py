32f999742b3b91e9953765924a4db705f1a2f34f
Trustin Nguyen <trustin.nguyen62@berkeley.edu>
2024-03-14 11:27:34 -0700

    Added Overflow Treatment

--- r_trees/r_star_tree.py
+++ r_trees/r_star_tree.py
@@ -18,6 +18,7 @@ class Bound(object):
             self.area = self.width * self.length
             self.center_x = self.min_x + self.length / 2
             self.center_y = self.min_y + self.width / 2
+            self.center = np.array([self.center_x, self.center_y])
             self.p_obj = None
 
     # returns perimeter
@@ -95,25 +96,30 @@ class IndexPointer(object):
         # self.bound.plot("#ff0000")
 
     def __repr__(self):
-        return "pt" + f"{self.bound} -> {self.pointer}"
+        return "pt " + f"{self.bound} -> {self.pointer}"
 
 
 class BranchNode(object):
-    def __init__(self, indices=[]):
+    def __init__(self, indices=[], covering=Bound(), level=0):
+        self.level = level
+        self.covering = covering
         self.items = indices
 
     def add_entry(self, entry):
         self.items.append(entry)
+        if self.covering:
+            self.covering = Bound.combine(self.covering, entry.bound)
 
     def __repr__(self):
         string = ""
         for i in self.items:
             string += str(i) + "\n"
-        return "Branch (\n" + textwrap.indent(string, "    ") + ")"
+        return "Branch " + f"{self.covering} " + "(\n" + textwrap.indent(string, "    ") + ")"
 
 
 class LeafNode(object):
-    def __init__(self, indices=[], covering=Bound()):
+    def __init__(self, indices=[], covering=Bound(), level=0):
+        self.level = level
         self.covering = covering
         if self.covering:
             covering.plot("#009b00")
@@ -136,6 +142,7 @@ class LeafNode(object):
     def rm_plot(self):
         for h in self.points:
             h.remove()
+        self.points = []
 
     def __repr__(self):
         string = ""
@@ -151,9 +158,12 @@ class RTree(object):
     ###########################################################################
 
     def __init__(self, M):
-        self.root = LeafNode(indices=[], covering=None)
+        self.root = LeafNode(indices=[], covering=None, level=0)
         self.max_num = M
         self.min_num = math.floor(M * .4)
+        self.height = 0
+        self.p = math.floor(M * .3)
+        self.overflow_levels = set()
 
     def __repr__(self):
         return "Root:\n" + textwrap.indent(f"{self.root}", "    ")
@@ -311,7 +321,34 @@ class RTree(object):
         self.ChooseSplitAxis(node)
         return self.ChooseSplitIndex(node.items)
 
-    def ChooseLeaf(self, node, index_entry):
+    def OverflowTreatment(self, node, index_entry, level):
+        if self.root.level > 0 and self.root.level not in self.overflow_levels:
+            self.overflow_levels.add(self.root.level)
+            self.Reinsert(node)
+            return None, None, None, None
+        else:
+            node.rm_plot()
+            node.covering.rm_plot()
+            l1, l2, b1, b2 = self.Split(node)
+            n1 = LeafNode(indices=l1, covering=b1, level=level)
+            n2 = LeafNode(indices=l2, covering=b2, level=level)
+            b1 = n1.covering
+            b2 = n2.covering
+            return n1, n2, b1, b2
+
+    def Reinsert(self, node):
+        node.rm_plot()
+        node.covering.rm_plot()
+        sort_dist = sorted(node.items, key=lambda x: np.linalg.norm(node.covering.center - x.bound.center), reverse=True)
+        node.items = sort_dist[self.p:]
+        node.covering = Bound()
+        for n in node.items:
+            node.covering = Bound.combine(node.covering, n.bound)
+        to_insert = sort_dist[:self.p][::-1]
+        for n in to_insert:
+            self.insert(n)
+
+    def ChooseLeaf(self, node, index_entry, level):
         n1, n2, b1, b2 = None, None, None, None
         if type(node) is LeafNode:
             if len(node.items) < self.max_num:
@@ -323,24 +360,18 @@ class RTree(object):
 
                 # if node is too big, split leaf node and add the entry to
                 # both each of the splits
-                node.rm_plot()
-                node.covering.rm_plot()
-                l1, l2, b1, b2 = self.Split(node)
-                n1 = LeafNode(indices=l1, covering=b1)
-                n2 = LeafNode(indices=l2, covering=b2)
-                n1.add_entry(index_entry)
-                n2.add_entry(index_entry)
-                b1 = n1.covering
-                b2 = n2.covering
+                node.add_entry(index_entry)
+                n1, n2, b1, b2 = self.OverflowTreatment(node, index_entry, level)
         else:
             # choosing parent of entry to insert
             idx_pointer, idx_pointer_pos = self.ChooseSubTree(node, index_entry)
 
             # index.pointer is the pointer to one of node's children nodes
-            n1, n2, b1, b2 = self.ChooseLeaf(idx_pointer.pointer, index_entry)
+            n1, n2, b1, b2 = self.ChooseLeaf(idx_pointer.pointer, index_entry, level - 1)
 
             # update bound
             idx_pointer.update(index_entry.bound)
+            node.covering = Bound.combine(node.covering, index_entry.bound)
 
             if n2:
 
@@ -355,104 +386,22 @@ class RTree(object):
 
                 # If the branch node has too many items split
                 l1, l2, b1, b2 = self.Split(node)
-                n1 = BranchNode(indices=l1)
-                n2 = BranchNode(indices=l2)
-                return n1, n2, b1, b2
+                n1 = BranchNode(indices=l1, covering=b1, level=level)
+                n2 = BranchNode(indices=l2, covering=b2, level=level)
 
         # returns either split leaf nodes, or branch nodes, depending on
         # which one is at the highest level of tree
         return n1, n2, b1, b2
 
-    # takes in a node to split, and minimum number of entries
-    # required in each new node.
-    # Returns two lists of nodes split from node.items
-    # along with their bounding box.
-    def SplitNode(self, node, m):
-        remaining = set(node.items)
-
-        # pick first element of our each new group
-        n1, n2, b1, b2 = [], [], Bound(), Bound()
-        r1, r2 = self.PickSeeds(node.items)
-        n1.append(r1), n2.append(r2)
-
-        remaining.remove(r1), remaining.remove(r2)
-        b1, b2 = r1.bound, r2.bound
-        while True:
-
-            if len(remaining) == 0:
-                break
-
-            if len(n1) >= m:
-                for s in remaining:
-                    n2.append(s)
-                    b2 = Bound.combine(b2, s.bound)
-                break
-
-            if len(n2) >= m:
-                for s in remaining:
-                    n1.append(s)
-                    b1 = Bound.combine(b1, s.bound)
-                break
-
-            r_new, pref = self.PickNext(remaining, b1, b2)
-            if pref == 1:
-                n1.append(r_new)
-                b1 = Bound.combine(b1, r_new.bound)
-            else:
-                n2.append(r_new)
-                b2 = Bound.combine(b2, r_new.bound)
-
-            remaining.remove(r_new)
-        return n1, n2, b1, b2
-
-    # when splitting the node, picks the first elements for
-    # our new list. These are the ones that are furthest apart
-    def PickSeeds(self, entries):
-        iefficieny = -math.inf
-        length = len(entries)
-        r1, r2 = None, None
-        for i in range(length):
-            for j in range(length - i - 1):
-                bounds = Bound.expand(entries[i].bound, entries[i + j + 1].bound)
-                exp_area = (bounds[1] - bounds[0]) * (bounds[2] - bounds[3])
-                curr_ieff = exp_area - entries[i].bound.area - entries[i + j + 1].bound.area
-                if curr_ieff > iefficieny:
-                    iefficieny = curr_ieff
-                    r1 = entries[i]
-                    r2 = entries[i + j + 1]
-        return r1, r2
-
-    # picks next entry to add to our split nodes based on which point
-    # has the most preference for a node
-    def PickNext(self, entries, b1, b2):
-        max_diff = -1
-        r = None
-        r_pref = 0
-        for entry in entries:
-            bound_1 = Bound.expand(entry.bound, b1)
-            bound_2 = Bound.expand(entry.bound, b2)
-            exp_area_1 = (bound_1[1] - bound_1[0]) * (bound_1[2] - bound_1[3])
-            exp_area_2 = (bound_2[1] - bound_2[0]) * (bound_2[2] - bound_2[3])
-            pref = 0
-            if exp_area_1 < exp_area_2:
-                pref = 1
-            else:
-                pref = 2
-            diff = abs(exp_area_1 - exp_area_2)
-            if diff > max_diff:
-                max_diff = diff
-                r = entry
-                r_pref = pref
-        return r, r_pref
-
     # inserts entry with propagation of changes upwards until root node
     # if root node is too big, we split
     def insert(self, index_entry):
-        n1, n2, b1, b2 = self.ChooseLeaf(self.root, index_entry)
+        n1, n2, b1, b2 = self.ChooseLeaf(self.root, index_entry, self.height)
         if n2:
-            self.root = BranchNode(indices=[])
+            self.height += 1
             p1 = IndexPointer(bound=b1, pointer=n1)
             p2 = IndexPointer(bound=b2, pointer=n2)
+            self.root = BranchNode(indices=[], covering=Bound.combine(b1, b2), level=self.height)
             self.root.add_entry(p1)
             self.root.add_entry(p2)
 
@@ -472,8 +421,8 @@ ax = plt.gca()
 ax.set_xlim([-10, 810])
 ax.set_ylim([-10, 810])
 
-rtree = RTree(100)
-for i in range(1000):
+rtree = RTree(20)
+for i in range(400):
     x, y = sample_point([0, 800, 800, 0])
     ti1 = np.array([x, y])
     b1 = Bound([x, x, y, y])
@@ -483,6 +432,13 @@ for i in range(1000):
 # Find out why it is always choosing the y-axis to split
 print(rtree)
 print("Done!")
+
+
+
+
+
+
+
 
 
 
